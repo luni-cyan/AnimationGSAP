f6c7ca75849e4baf13c65b6ba5440207c82bb849
luni <zoka105105@gmail.com>
2024-10-14 12:04:31 +0800

    feat:增加 timeline 使用方法

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Flip from './components/Flip.vue'
+import Timeline from './components/Timeline.vue'
 </script>
 
 <template>
   <Flip />
+  <Timeline />
 </template>
 
 <style scoped></style>
--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -6,9 +6,9 @@ const container = ref(null)
 let _ctx
 let tl
 onMounted(() => {
-  ctx = gsap.context(() => {
+  _ctx = gsap.context(() => {
     const boxes = gsap.utils.toArray('.box')
-    tl = gsap.timeline({ paused: true }).to(boxes[0], { x: 100 }).to(boxes[1], { x: -100 }, '<').to(boxes[2], { y: -80 })
+    tl = gsap.timeline({ paused: true }).to(boxes[0], { x: 100 }).to(boxes[1], { x: -100 }).to(boxes[2], { y: -80, opacity: 0 }, 1).to(boxes[3], { x: 200 }, '<', 'test').to(boxes[4], { y: -200 })
   }, container.value)
 })
 
@@ -20,6 +20,18 @@ function toggleTimeLine() {
     tl.reverse()
   }
 }
+function reverseTimeLine() {
+  tl.reverse()
+}
+function restartTimeLine() {
+  tl.restart()
+}
+function pauseTimeLine() {
+  tl.pause()
+}
+function skipTimeLine() {
+  tl.play('test')
+}
 </script>
 
 <template>
@@ -27,6 +39,18 @@ function toggleTimeLine() {
     <div>
       <button @click="toggleTimeLine">
         Toggle
+      </button>
+      <button @click="reverseTimeLine">
+        reverse
+      </button>
+      <button @click="restartTimeLine">
+        restart
+      </button>
+      <button @click="pauseTimeLine">
+        pause
+      </button>
+      <button @click="skipTimeLine">
+        skip
       </button>
     </div>
     <div ref="container" class="container">
@@ -38,6 +62,12 @@ function toggleTimeLine() {
       </div>
       <div class="box blue">
         Box 3
+      </div>
+      <div class="box green">
+        Box 4
+      </div>
+      <div class="box red">
+        Box 5
       </div>
     </div>
   </section>
